d0d1c9a8e84c2c42fd96c3bb659a314ea6711767
Priyranjan89 <37174924+Priyranjan89@users.noreply.github.com>
2025-04-10 23:12:11 +0530

    Spring Boot Intital commit

---
+++ src/main/java/com/learn/spring/boot/introduction_spring_boot/IntroductionSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.learn.spring.boot.introduction_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionSpringBootApplication.class, args);
+	}
+
+}
